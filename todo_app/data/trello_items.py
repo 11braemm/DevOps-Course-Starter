e22393b4caf96d07fd25ade8e5e8fec257e4edb6
11braemm <emmacarolinebragg@gmail.com>
2024-02-14 12:55:47 +0000

    Module 2 feat: full excerice

---
+++ todo_app/data/trello_items.py
@@ -0,0 +1,75 @@
+import requests
+import os
+
+from todo_app.data.Item import Item
+
+def add_item(title):
+    print(title)
+    """
+    Adds a new item with the specified title to the session.
+
+    Args:
+        title: The title of the item.
+
+    Returns:
+        item: The saved item.
+    """
+    reqUrl = (
+        f"https://api.trello.com/1/cards?"
+        f"idList={os.getenv('NOT_STARTED_LIST_ID')}&"
+        f"key={os.getenv('TRELLO_API_KEY')}&"
+        f"token={os.getenv('TRELLO_API_TOKEN')}&"
+        f"name={title}"
+    )
+
+    headersList = {
+    "Accept": "*/*",
+    "User-Agent": "Thunder Client (https://www.thunderclient.com)" 
+    }
+
+    payload = ""
+
+    requests.request("POST", reqUrl, data=payload,  headers=headersList)
+
+def get_items():
+    reqUrl = (
+        f"https://api.trello.com/1/boards/"
+        f"{os.getenv('BOARD_ID')}/cards?"
+        f"key={os.getenv('TRELLO_API_KEY')}&"
+        f"token={os.getenv('TRELLO_API_TOKEN')}"
+    )
+
+    headersList = {
+    "Accept": "*/*",
+    "User-Agent": "Thunder Client (https://www.thunderclient.com)" 
+    }
+
+    payload = ""
+
+    response = requests.request("GET", reqUrl, data=payload,  headers=headersList)
+
+    return [ Item.from_trello_card(item) for item in response.json()]
+    
+def mark_item_as_done(card_id):
+    """
+    Marks the Trello card with the specified ID as done by moving it to the "Done" list.
+
+    Args:
+        card_id: The ID of the Trello card to mark as done.
+    """
+    reqUrl = (
+        f"https://api.trello.com/1/cards/{card_id}?"
+        f"idList={os.getenv('DONE_LIST_ID')}&"  # Change to your "Done" list ID
+        f"key={os.getenv('TRELLO_API_KEY')}&"
+        f"token={os.getenv('TRELLO_API_TOKEN')}"
+    )
+
+    headersList = {
+        "Accept": "*/*",
+        "User-Agent": "Thunder Client (https://www.thunderclient.com)"
+    }
+
+    payload = ""
+
+    requests.request("PUT", reqUrl, data=payload, headers=headersList)
+    

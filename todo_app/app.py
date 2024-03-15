dae43e1a4269f3fb044cd99a0ff8838d2128b671
11braemm <emmacarolinebragg@gmail.com>
2024-03-15 17:51:40 +0000

    module 3 style: run lint

--- todo_app/app.py
+++ todo_app/app.py
@@ -15,18 +15,15 @@ def create_app():
         item_view_model = ViewModel(items)
         return render_template("index.html", view_model=item_view_model)
 
-
     @app.route("/add-item", methods=["POST"])
     def login():
         new_item = request.form["title"]
         add_item(new_item)
         return redirect("/")
 
-
     @app.route("/complete-item/<item_id>", methods=["POST"])
     def complete_item(item_id):
         mark_item_as_done(item_id)
         return redirect("/")
-    
 
     return app
